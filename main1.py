258e23c1da0e1723e1eeaa607ee2797fcf1e7381
lakkaloko <rodrigolacombe@gmail.com>
2023-07-24 08:20:15 -0300

    loops

--- main1.py
+++ main1.py
@@ -7,16 +7,35 @@ import time
 
 url = input("Informe a URL: ")
 
+contador = 0
+
 def view_video():
   
+  global contador
+
   options = webdriver.FirefoxOptions()
   options.headless = True
   driver = webdriver.Firefox(options=options)
+  
+
+  print("iniciando driver...")
 
   driver.get(url)
   driver.minimize_window()
 
   duration = int(driver.execute_script("return document.getElementById('movie_player').getDuration()"))
+
+while True:
+
+  time.sleep(randint(3,5))
+  webdriver.refresh()
+
+  contador += 1
+
+if contador % 10 == 0:
+  print(f"Views: {contador}")
+    
+  print("Finalizando driver...")
 
   watched = 0
   while watched < duration * randint(25, 100) / 100:
@@ -43,4 +62,6 @@ if __name__ == "__main__":
     for driver in drivers:
       driver.join()
     
-    print("Rodada completa!")
+print(f"Total views: {contador}")
+
+time.sleep(60)
